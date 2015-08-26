e7f7e6a20e68dd4392095826cfe08430e5b5ba79
Kishon Daniels <kishondaniels@gmail.com>
2015-08-26 11:05:11 -0500

    Update SumProduct.c

--- SumProduct.c
+++ SumProduct.c
@@ -5,8 +5,7 @@ int main (void)
 {
     int integer1;
     int integer2;
-    int 1;
-    int 2;
+    int a;
     
     printf("Enter a number");
     scanf("%d", &integer1);
@@ -15,26 +14,26 @@ int main (void)
     scanf("%d", &integer2);
     
     printf("Enter 1 to add or 2 to multiply");
-    scanf("%d", int 1, int 2);
+    scanf("%d", &a);
     
-    sum = &integer1 + &integer2;
-    product = &integer1 * &integer2;
-    
-    
-    if (int 1)
+    if (a==1)
     {
         
-        printf("the sum of %d and %d is %d", sum);
-        
+        sum = integer1 + integer2;
+        printf("sum is %d \n,", sum);
+    
         
     }
     
-    else if (int 2)
+    else if (a==2)
     {
         
-        printf("the product of %d and %d is %d", product);
+        product = integer1 * integer2;
+        printf("product is %d \n", product);
+        
         
     }
+
     
     
     return 0;
